4f25f5b311f6f436efc784137a2661f85b4a0cc5
Menna Nabil <mena99nabil@gmail.com>
2024-12-17 23:47:00 +0200

    -added images and svgs -added colors -added styles -create onboarding screen -added routes and router -added flutter_svg and screenutil

---
+++ lib/doc_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medical_app_adv/core/routing/app_router.dart';
+import 'package:medical_app_adv/core/routing/routes.dart';
+import 'package:medical_app_adv/core/theming/colors.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      designSize: const Size(375,812),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: AppColors.mainBlue,
+          scaffoldBackgroundColor: Colors.white
+        ),
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
---
+++ lib/features/on_boarding/widgets/get_started_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:medical_app_adv/core/helpers/extinsions.dart';
+import 'package:medical_app_adv/core/routing/routes.dart';
+import 'package:medical_app_adv/core/theming/colors.dart';
+import 'package:medical_app_adv/core/theming/styles.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: TextButton(
+          onPressed: (){
+            context.pushNamed(Routes.loginScreen);
+          },
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(AppColors.mainBlue),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              maximumSize: MaterialStateProperty.all(const Size(double.infinity,52)),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)))
+          ),
+          child: Text(
+            'Get Started',
+            style: TextStyles.font16whiteSemiBold,)),
+    );
+  }
+}
